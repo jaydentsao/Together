29a49b1773dfba643a74abebea6eb880d40b31d2
boi <tsaojayden@icloud.com>
2025-04-07 09:23:34 -0400

    Changed Server to send to all servers and to support variable amounts of players

--- Server.java
+++ Server.java
@@ -1,30 +1,26 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Server
 {
-    private ServerThread thread1;
-    private ServerThread thread2;
+    private ServerThread[] threads;
 
     public Server()
     {
         try
         {
+            ArrayList<ServerThread> list=new ArrayList<>();
+            while (true){
+                long time=System.currentTimeMillis();
             //start listening for connections on port 9000
-            ServerSocket serverSocket = new ServerSocket(9000);
+                ServerSocket serverSocket = new ServerSocket(9000);
 
-            //accept connection from player 1
-            Socket socket1 = serverSocket.accept();
-            //create ServerThread for handling connection for player 1
-            thread1 = new ServerThread(socket1, this, 1);
-
-            //accept connection from player 2
-            Socket socket2 = serverSocket.accept();
-            //create ServerThread for handling connection for player 2
-            thread2 = new ServerThread(socket2, this, 2);
-
-            //tell player 1 to go first
-            thread1.send("go");
+                //create ServerThread for handling connection for player 1
+                list.add(new ServerThread(serverSocket.accept(), this, 1));
+                if(System.currentTimeMillis()-time>10000) break;
+            }
+            threads=(ServerThread[])((list.toArray()));
         }
         catch(IOException e)
         {
@@ -33,11 +29,10 @@ public class Server
     }
 
     //send message to other player
-    public void sendToOpponent(String message, int playerNum)
+    public void sendToAll(String message)
     {
-        if (playerNum == 1)
-            thread2.send(message);
-        else
-            thread1.send(message);
+        for (ServerThread thread : threads) {
+            thread.send(message);
+        }
     }
 }
