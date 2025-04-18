05089cc0a99d7727537a17dc7033dca4003212e4
Adam Aikins <adamminecraftman@gmail.com>
2025-04-18 09:53:33 -0400

    cleaned

--- Display.java
+++ Display.java
@@ -9,16 +9,15 @@ import java.util.*;
 
 
 public class Display extends JComponent implements KeyListener, MouseListener {
-    private Image lebron;
-    private Image grid;
-    private Image[] colorsImages;
+    private final Image lebron;
+    private final Image grid;
+    private final Image[] colorsImages;
 
     private int width;
     private int height;
 
     LinkedList<Obstacle> obstacles;
     ArrayList<Player> players;
-    double playerSizeP;
 
     int playerSize;
 
@@ -132,14 +131,6 @@ public class Display extends JComponent implements KeyListener, MouseListener {
             double colorHeight = colorWidth / (double)colorsImages[color].getWidth(null) * (double)colorsImages[color].getHeight(null);
             g.drawImage(colorsImages[color], (int)((double)(width / 2) - colorWidth / (double)2.0F), (int)((double)(height / 2) - colorHeight / (double)2.0F), (int)colorWidth, (int)colorHeight, null);
         }
-
-        // red rectangle on lebron image's border
-        // Graphics2D g2d = (Graphics2D) g;
-        // g2d.setColor(Color.RED);
-        // g2d.setStroke(new BasicStroke(3));
-        // g2d.drawRect(playerX, playerY, 50, 50);
-        // g2d.setColor(Color.GREEN);
-        // g2d.drawRect(player2X, player2Y, 50, 50);
     }
 
     public void run() {
@@ -147,12 +138,7 @@ public class Display extends JComponent implements KeyListener, MouseListener {
             Dimension size = frame.getSize();
             width = size.width;
             height = size.height;
-            if (!start) {
-                try {
-                    Thread.sleep(1);
-                } catch (Exception e) {
-                }
-            } else {
+            if (start) {
                 if (directions[0] && !jumping)
                     jumping = true;
                 if (directions[1])
@@ -169,22 +155,21 @@ public class Display extends JComponent implements KeyListener, MouseListener {
                         currentJumpHeight = 0;
                     } else {
                         currentJumpHeight++;
-                        players.get(playerNum - 1).setY(players.get(playerNum - 1).getY()-1);
+                        players.get(playerNum - 1).setY(players.get(playerNum - 1).getY() - 1);
                     }
                 }
 
                 // Gravity
                 if (falling && allowMove[2]) {
                     if (gravity != jumpHeight) {
-                        players.get(playerNum - 1).setY(players.get(playerNum - 1).getY()+1);
+                        players.get(playerNum - 1).setY(players.get(playerNum - 1).getY() + 1);
                         gravity++;
-                    }
-                    else {
+                    } else {
                         falling = false;
                         gravity = 0;
                     }
                 }
-                allowMove = new boolean[] { true, true, true, true };
+                allowMove = new boolean[]{true, true, true, true};
                 // Player-Player Collisions
                 for (int i = 0; i < numPlayers; i++) {
                     if (i != playerNum - 1) {
@@ -216,28 +201,28 @@ public class Display extends JComponent implements KeyListener, MouseListener {
                 }
                 // Player-Obstacle Collisions
                 for (Obstacle obstacle : obstacles) {
-                    int[] coords=obstacle.getCoords();
+                    int[] coords = obstacle.getCoords();
                     // Left Border
                     if (players.get(playerNum - 1).getX() == coords[2]
-                            && (players.get(playerNum - 1).getY() +playerSize<coords[1] || players.get(playerNum - 1).getY()-playerSize>coords[3])){
+                            && (players.get(playerNum - 1).getY() + playerSize < coords[1] || players.get(playerNum - 1).getY() - playerSize > coords[3])) {
                         directions[1] = false;
                         allowMove[1] = false;
                     }
                     // Right Border
-                    if (players.get(playerNum - 1).getX()  + playerSize == coords[0]
-                            && (players.get(playerNum - 1).getY()+playerSize<coords[1] || players.get(playerNum - 1).getY()-playerSize>coords[3])) {
+                    if (players.get(playerNum - 1).getX() + playerSize == coords[0]
+                            && (players.get(playerNum - 1).getY() + playerSize < coords[1] || players.get(playerNum - 1).getY() - playerSize > coords[3])) {
                         directions[3] = false;
                         allowMove[3] = false;
                     }
                     // Top Border
                     if (players.get(playerNum - 1).getY() == coords[3]
-                            && (players.get(playerNum - 1).getX() +playerSize<coords[0] || players.get(playerNum - 1).getX() -playerSize>coords[2])) {
+                            && (players.get(playerNum - 1).getX() + playerSize < coords[0] || players.get(playerNum - 1).getX() - playerSize > coords[2])) {
                         directions[0] = false;
                         allowMove[0] = false;
                     }
                     // Bottom Border
                     if (players.get(playerNum - 1).getY() + playerSize == coords[1]
-                            && (players.get(playerNum - 1).getX() +playerSize<coords[0] || players.get(playerNum - 1).getX() -playerSize>coords[2])) {
+                            && (players.get(playerNum - 1).getX() + playerSize < coords[0] || players.get(playerNum - 1).getX() - playerSize > coords[2])) {
                         directions[2] = false;
                         allowMove[2] = false;
                     }
@@ -248,34 +233,30 @@ public class Display extends JComponent implements KeyListener, MouseListener {
                 repaint();
                 client.send("pos " + (players.get(playerNum - 1)).getX() + " " + (players.get(playerNum - 1)).getY() + " " + playerNum);
 
-                try {
-                    Thread.sleep(1);
-                } catch (Exception e) {
-                }
             }
+            try { Thread.sleep(1); }
+            catch (Exception _) { }
         }
     }
 
     public void updatePosition(int x, int y, int player) {
-        (players.get(player - 1)).setX(x);
-        (players.get(player - 1)).setY(y);
+        players.get(player - 1).setX(x);
+        players.get(player - 1).setY(y);
     }
 
     public void updateColor(int c, int player) {
-        (players.get(player - 1)).setColor(c);
+        players.get(player - 1).setColor(c);
     }
 
     public void readyGame(int numPlayers, int playerNum) {
         ready = true;
         this.numPlayers = numPlayers;
         this.playerNum = playerNum;
-
         for(int i = 0; i < numPlayers; ++i) {
             int[] playerPosition = new int[]{i*(playerSize+5), 0};
             players.add(new Player(playerPosition, i + 1, 0));
         }
-
-        System.out.println("ready");
+        //System.out.println("ready");
     }
 
     public void start() {
@@ -284,6 +265,7 @@ public class Display extends JComponent implements KeyListener, MouseListener {
 
     public void keyPressed(KeyEvent e) {
         // System.out.println(e.getKeyCode());
+        //movement
         if (e.getKeyCode() == 87 && !allowMove[2]) // W -> Up
             directions[0] = true;
         if (e.getKeyCode() == 65 && allowMove[1]) // A -> Left
@@ -293,43 +275,37 @@ public class Display extends JComponent implements KeyListener, MouseListener {
         if (e.getKeyCode() == 68 && allowMove[3]) // D -> Right
             directions[3] = true;
 
-
-        if (ready && e.getKeyCode() == 32) {
-            HashSet<Integer> colorSet = new HashSet();
-
-            for(int i = 0; i < numPlayers; ++i) {
-                colorSet.add((players.get(i)).getColor());
-            }
-
-            if (colorSet.size() == numPlayers) {
-                client.send("start");
-                start();
-            }
-        }
-
+        //color select
         if (!start && ready) {
             if (e.getKeyCode() == 39) {
-                if (color == 9) {
+                if (color == 9)
                     color = 0;
-                } else {
-                    ++color;
-                }
-
+                else
+                    color++;
                 repaint();
                 client.send("color " + color + " " + playerNum);
-                (players.get(playerNum - 1)).setColor(color);
+                players.get(playerNum - 1).setColor(color);
             }
 
             if (e.getKeyCode() == 37) {
-                if (color == 0) {
+                if (color == 0)
                     color = 9;
-                } else {
-                    --color;
-                }
-
+                else
+                    color--;
                 repaint();
                 client.send("color " + color + " " + playerNum);
-                (players.get(playerNum - 1)).setColor(color);
+                players.get(playerNum - 1).setColor(color);
+            }
+        }
+
+        //game start
+        if (ready && e.getKeyCode() == 32) {
+            HashSet<Integer> colorSet = new HashSet<Integer>();
+            for(int i = 0; i < numPlayers; ++i)
+                colorSet.add((players.get(i)).getColor());
+            if (colorSet.size() == numPlayers) {
+                client.send("start");
+                start();
             }
         }
 }
@@ -346,21 +322,10 @@ public class Display extends JComponent implements KeyListener, MouseListener {
             directions[3] = false;
     }
 
-    public void mouseClicked(MouseEvent e) {
-    }
-
-    public void mousePressed(MouseEvent e) {
-    }
-
-    public void mouseReleased(MouseEvent e) {
-    }
-
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    public void mouseExited(MouseEvent e) {
-    }
-
-    public void keyTyped(KeyEvent e) {
-    }
+    public void mouseClicked(MouseEvent e) {}
+    public void mousePressed(MouseEvent e) {}
+    public void mouseReleased(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
+    public void mouseExited(MouseEvent e) {}
+    public void keyTyped(KeyEvent e) {}
 }
