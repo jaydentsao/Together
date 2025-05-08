d4d17392aefc9ad0f7b8cd819615462edf9dacdb
Jayden Tsao <tsaojayden@icloud.com>
2025-05-08 15:16:20 -0400

    adding edit

--- Main.java
+++ Main.java
@@ -3,15 +3,13 @@ public class  Main
     public static void main(String[] args)
     {
         //if hosting a game run this code:
+        //joinGame("10.13.30.46");
         hostGame(2);
         joinGame("localhost");
         joinGame("localhost");
-//        joinGame("localhost");
-//        joinGame("localhost");
         //joinGame("localhost");
 //        joinGame("169.254.253.11");
 
-        
         //if joining a game, run this code with host's IP address:
         //joinGame("10.13.30.46");
     }
